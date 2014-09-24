684e875a0fd8be9a3a44f450845da71ef9fdda29
Ben Dixon <ben@talkingquickly.co.uk>
2014-09-25 00:01:36 +0100

    test rails env from environment var

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,5 +4,5 @@ env :RAILS_ENV, ENV['RAILS_ENV']
 
 
 every 1.minutes, roles: [:primary]  do
-  command "cd #{path}/backup && RAILS_ENV=#{RAILS_ENV} bundle exec backup perform -t rails_database --config_file ./config.rb"
+  command "cd #{path}/backup && RAILS_ENV=#{ENV['RAILS_ENV']} bundle exec backup perform -t rails_database --config_file ./config.rb"
 end
