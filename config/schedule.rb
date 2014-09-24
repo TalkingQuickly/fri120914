3f8c4db2111a83bc6971be75e28f4bc294fcb7f0
Ben Dixon <ben@talkingquickly.co.uk>
2014-09-25 00:13:36 +0100

    whenever test

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, "#{path}/log/cron_log.log"
 env :PATH, ENV['PATH']
 
-every 1.minutes  do
+every 2.minutes do
   command "cd #{path}/backup && RAILS_ENV=#{fetch(:whenever_environment)} bundle exec backup perform -t rails_database --config_file ./config.rb"
 end
